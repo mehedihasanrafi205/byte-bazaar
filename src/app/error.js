75b0e9cfb1deb7db8bac7b759f99e010a370c785
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-11-26 03:59:27 +0600

    Update project

--- src/app/error.js
+++ src/app/error.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
  
 export default function Error({ error, reset }) {
   useEffect(() => {
-    // Log the error to an error reporting service
+   
     console.error(error)
   }, [error])
  
@@ -13,7 +13,7 @@ export default function Error({ error, reset }) {
       <h2>Something went wrong!</h2>
       <button
         onClick={
-          // Attempt to recover by trying to re-render the segment
+          
           () => reset()
         }
       >
