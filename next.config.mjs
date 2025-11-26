cb05a0dfa8b67752a4d03b2accbd956fcda622be
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-11-27 00:10:32 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
       { protocol: "https", hostname: "img.freepik.com", pathname: "/**" },
       { protocol: "https", hostname: "via.placeholder.com", pathname: "/**" },
       { protocol: "https", hostname: "gadgetnmusic.com", pathname: "/**" },
+      { protocol: "https", hostname: "i.ibb.co.com", pathname: "/**" },
     ],
   },
 };
